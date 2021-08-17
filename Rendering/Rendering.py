097526d15244fce54fefbfe32617f1df98f67bf2
Paransaik <jty419@naver.com>
2021-08-18 08:09:31 +0900

    8.18. modify voxel, rendering

--- Rendering/Rendering.py
+++ Rendering/Rendering.py
@@ -1,9 +1,7 @@
 import vtk
 
 class VolumeRendering:
-
     def __init__(self, path):
-        
         self.reader = vtk.vtkDICOMImageReader()
         self.reader.SetDirectoryName(r'{}'.format(path))
         self.reader.Update()
@@ -22,30 +20,25 @@ class VolumeRendering:
         self.actor()
         self.rendering()
 
-    def mapper(self):
+    def property(self):
+        opacityWindow = 4096  # 값 바꿔가며 어떤 걸 변경하는 값인지 확인해보기
+        opacityLevel = 2048  # ...
 
+        self.c_f.AddRGBSegment(0.0, 1.0, 1.0, 1.0, 255.0, 1.0, 1.0, 1.0)
+        self.o_f.AddSegment(opacityLevel - 0.5 * opacityWindow, 0.0, opacityLevel + 0.5 * opacityWindow, 1.0)
+
+        self.p.SetColor(self.c_f)
+        self.p.SetScalarOpacity(self.o_f)
+
+    def mapper(self):
         self.m.SetInputConnection(self.reader.GetOutputPort())
         self.m.SetBlendModeToMaximumIntensity()
     
-    def property(self):
-
-        opacityWindow = 4096 # 값 바꿔가며 어떤 걸 변경하는 값인지 확인해보기
-        opacityLevel = 2048 # ...
-
-        self.c_f.AddRGBSegment(0.0, 1.0, 1.0, 1.0, 255.0, 1.0, 1.0, 1.0)
-        self.o_f.AddSegment(opacityLevel - 0.5*opacityWindow, 0.0,
-            opacityLevel + 0.5*opacityWindow, 1.0)
-        
-        self.p.SetColor(self.c_f)
-        self.p.SetScalarOpacity(self.o_f) 
-    
     def actor(self):
-        
         self.v.SetMapper(self.m)
         self.v.SetProperty(self.p)
     
     def rendering(self):
-        
         self.renWin.AddRenderer(self.ren)
         self.renWin.SetSize(600, 600)
         self.iren.SetRenderWindow(self.renWin)
@@ -56,9 +49,7 @@ class VolumeRendering:
         self.iren.Start()
 
 class SliceRendering:
-
     def __init__(self, path):
-
         self.reader = vtk.vtkDICOMImageReader()
         self.reader.SetFileName(r'{}'.format(path))
         self.reader.Update()
@@ -76,11 +67,9 @@ class SliceRendering:
         self.actor()
         self.rendering()
 
-
     def source(self):
         # data extent(데이터 범위)
-        (xMin, xMax, yMin, yMax, zMin, zMax) = self.reader.GetExecutive().GetWholeExtent( 
-                                    self.reader.GetOutputInformation(0))
+        (xMin, xMax, yMin, yMax, zMin, zMax) = self.reader.GetExecutive().GetWholeExtent(self.reader.GetOutputInformation(0))
         (xSpacing, ySpacing, zSpacing) = self.reader.GetOutput().GetSpacing() 
         (x0, y0, z0) = self.reader.GetOutput().GetOrigin() 
 
--- voxel.py
+++ voxel.py
@@ -12,47 +12,44 @@ class PyVoxel: # Voxel은 Volumn과 pixel의 합성어
     # 이 파일의 내용은 바이너리 형태이다. 
     # 텍스트 파일이아닌 컴퓨터 파일이다. 
     # 바이너리파일은 '한 줄에 해당하는 데이터를 읽어라'와 같은 명령을 사용하지 않고 '10 바이트를 읽어라'로 사용
-
     def __init__(self):
-        self.m_Org = -1 # 지금 신경안써도된다. 다 255로 되어있을 것이다. , 순서가 뒤바꼈을 경우를 대비한것이다.
-        self.m_nX = 0 # 픽셀 개수
-        self.m_nY = 0 # ...
-        self.m_nZ = 0 #... 
+        self.m_Org = -1  # 지금 신경안써도된다. 다 255로 되어있을 것이다. , 순서가 뒤바꼈을 경우를 대비한것이다.
+        self.m_nX = 0  # 픽셀 개수
+        self.m_nY = 0  # ...
+        self.m_nZ = 0  #... # 슬라이스 개수
 
-        self.m_fXSp=1.0 # Spacing(sp) : 사진끼리 떨어져있는 거리(x)
-        self.m_fYSp=1.0 # ...(y)
-        self.m_fZSp=1.0 # ...(z)
+        self.m_fXSp = 1.0  # Spacing(sp) : 사진끼리 떨어져있는 거리(x) # row 파일 픽셀 개수
+        self.m_fYSp = 1.0  # ...(y)
+        self.m_fZSp = 1.0  # ...(z)
 
-        self.m_fXOrg=-1.0 # Org면 오리지널? 
-        self.m_fYOrg=-1.0
-        self.m_fZOrg=-1.0
-
+        self.m_fXOrg = -1.0  # Org면 오리지널?
+        self.m_fYOrg = -1.0
+        self.m_fZOrg = -1.0
         self.m_Voxel = []
 
-    def initialize(self): # 변수들의 초기 내용을 설정 
+    def initialize(self):  # 변수들의 초기 내용을 설정
         self.m_Org = -1
         self.m_nX = 0
         self.m_nY = 0
         self.m_nZ = 0
 
-        self.m_fXSp = 1.0 # Spacing(sp) : 사진끼리 떨어져있는 거리(x)
-        self.m_fYSp = 1.0 # ...(y)
-        self.m_fZSp = 1.0 # ...(z)
+        self.m_fXSp = 1.0  # Spacing(sp) : 사진끼리 떨어져있는 거리(x)
+        self.m_fYSp = 1.0  # ...(y)
+        self.m_fZSp = 1.0  # ...(z)
 
         self.m_fXOrg = -1.0
         self.m_fYOrg = -1.0
         self.m_fZOrg = -1.0
         self.m_Voxel = []
 
-
-    def ReadFromRaw(self, filename): # 이미지 16비트
-        with open(filename, 'rb') as f: # rb = byte 형식으로 파일 읽기 # f.close를 매번하기 귀찮기 때문에 with를 사용, f는 instance
+    def ReadFromRaw(self, filename):  # 이미지 16비트
+        with open(filename, 'rb') as f:  # rb = byte 형식으로 파일 읽기 # f.close를 매번하기 귀찮기 때문에 with를 사용, f는 instance
             try:
-                Header = np.tofile(f, dtype='int32', count=1) # dtype이 int32인 data만 file로부터 받아오는?
-                self.m_Org = Header[0] # header는 Raw파일에 첫 두바이트를 m_org를 넣었다. 저게 -1이라면 
+                Header = np.tofile(f, dtype='int32', count=1)  # dtype이 int32인 data만 file로부터 받아오는?
+                self.m_Org = Header[0]  # header는 Raw파일에 첫 두바이트를 m_org를 넣었다. 저게 -1이라면
                                         # 만약 -1이 아니라면 숫자란 이야기다 256이 들어있다.
-                if self.m_Org == -1: # 데이터가 거꾸로 뒤집어진? 경우 
-                    Header = np.fromfile(f, dtype='float32', count=6) # "x, y, z spacing", "x, y, z orgin"
+                if self.m_Org == -1:  # 데이터가 거꾸로 뒤집어진? 경우
+                    Header = np.fromfile(f, dtype='float32', count=6)  # "x, y, z spacing", "x, y, z orgin"
 
                     self.m_fXSp = Header[0]
                     self.m_fYSp = Header[1]
@@ -62,12 +59,12 @@ class PyVoxel: # Voxel은 Volumn과 pixel의 합성어
                     self.m_fYOrg = Header[4]
                     self.m_fZOrg = Header[5]
 
-                    Header = np.fromfile(f, dtype='int32', count=1) # "nX"
+                    Header = np.fromfile(f, dtype='int32', count=1)  # "nX"
                     self.m_nX = Header[0]
-                else: # 정상일 경우
-                    self.m_nX = self.m_Org # m_nX는 X크기 but, Header[1] = m_nX 값일텐데 왜? 
+                else:  # 정상일 경우
+                    self.m_nX = self.m_Org  # m_nX는 X크기 but, Header[1] = m_nX 값일텐데 왜?
 
-                Header = np.fromfile(f, dtype='int32', count=2) # nY nZ
+                Header = np.fromfile(f, dtype='int32', count=2)  # nY nZ
                 self.m_nY = Header[0]
                 self.m_nZ = Header[1]
 
@@ -78,8 +75,9 @@ class PyVoxel: # Voxel은 Volumn과 pixel의 합성어
                 print('Could not read file ' + filename)
                 sys.exit()
 
-    def ReadFromBin(self,filename): # .bin 파일 읽는
-            with open(filename,'rb') as f:
+    def ReadFromBin(self, filename): # .bin 파일 읽는
+            with open(filename, 'rb') as f:
+                # print('success ReadFromBin')
                 try:
                     Header = np.fromfile(f,dtype='int32', count=1)
                     self.m_Org = Header[0]
@@ -115,18 +113,18 @@ class PyVoxel: # Voxel은 Volumn과 pixel의 합성어
         # 이미지를 출력할 때 반드시 필요한 정보가있다. 그래서 사용자의 정보 입력이 없이는 raw파일을 출력할 수 없다.
         print('success WriteToRaw')
         print(filename)
-        HeaderDim = np.array([self.m_Org, self.m_nX, self.m_nY, self.m_nZ], dtype=np.int32) # shape = (4,)
+        HeaderDim = np.array([self.m_Org, self.m_nX, self.m_nY, self.m_nZ], dtype=np.int32)  # shape = (4,)
         HeaderSpOrg = np.array([self.m_fXSp, self.m_fYSp, self.m_fZSp, self.m_fXOrg, self.m_fYOrg, self.m_fZOrg], dtype=np.float32) # shape = (6,)
         print(HeaderDim)
         print(HeaderSpOrg)
 
-        Save = self.m_Voxel.astype(np.int16, copy=False) # if the type of m_voxel is np.int16, copy operation isn't perform
+        Save = self.m_Voxel.astype(np.int16, copy=False)  # if the type of m_voxel is np.int16, copy operation isn't perform
 
-        with open(filename,'wb') as f: # 쓰기 전용으로 파일 오픈
-            HeaderDim[0].tofile(f) # self.m_Org를 넣는다.
-            HeaderSpOrg.tofile(f) # HeaderSpOrg
-            HeaderDim[1:].tofile(f) # HeaderDim의 값을 넣는다.
-            Save.tofile(f) # .
+        with open(filename, 'wb') as f:  # 쓰기 전용으로 파일 오픈
+            HeaderDim[0].tofile(f)  # self.m_Org를 넣는다.
+            HeaderSpOrg.tofile(f)  # HeaderSpOrg
+            HeaderDim[1:].tofile(f)  # HeaderDim의 값을 넣는다.
+            Save.tofile(f)  # .
             
     def WriteToBin(self, filename):
         print('success WriteToBin')
@@ -141,8 +139,7 @@ class PyVoxel: # Voxel은 Volumn과 pixel의 합성어
             HeaderDim[1:].tofile(f)
             Save.tofile(f)
 
-
-    def AdjustPixelRange(self, Lower, Upper): # WL, WW를 설정
+    def AdjustPixelRange(self, Lower, Upper):  # WL, WW를 설정
         # version 2 
         range_ratio = (Upper - Lower) / 256.0
         pData = self.m_Voxel
@@ -151,22 +148,21 @@ class PyVoxel: # Voxel은 Volumn과 pixel의 합성어
         img_adjusted = img_adjusted.clip(0, 255)
         return img_adjusted
 
-
-    def NumpyArraytoVoxel(self, data): # 넘파이 배열을 받으면   
+    def NumpyArraytoVoxel(self, data):  # 넘파이 배열을 받으면
         self.initialize()
 
-        Dim = data.shape # shape = (,)
-        self.m_nX = Dim[2] # shape는 (가로, 세로)가 아닌 (세로, 가로)의 순서이기 때문 
+        Dim = data.shape  # shape = (,)
+        self.m_nX = Dim[2]  # shape는 (가로, 세로)가 아닌 (세로, 가로)의 순서이기 때문
         self.m_nY = Dim[1]
         self.m_nZ = Dim[0] 
         
-        self.m_Voxel = data.astype(np.int16, copy = False) # .append로하면 겉에는 list가 된다.
+        self.m_Voxel = data.astype(np.int16, copy=-False)  # .append로하면 겉에는 list가 된다.
 
     def ConvertValue(self, SrcV, TarV): # 값을 변환시키는 함수? 
         idx = self.m_Voxel == SrcV
         self.m_Voxel[idx] = TarV
 
-    def SaveWithoutHeader(self, filename): # Header 없이 저장하는 버전 
+    def SaveWithoutHeader(self, filename):  # Header 없이 저장하는 버전
         print('success SaveWithoutHeader')
         Save = self.m_Voxel.astype(np.uint8, copy=False)
         print(Save)
@@ -175,20 +171,20 @@ class PyVoxel: # Voxel은 Volumn과 pixel의 합성어
 
     def Normalize(self):
         self.m_Voxel = self.m_Voxel.astype(np.float32, copy=False)
-        maxvalue = np.max(self.m_Voxel) # m_Voxel의 가장 큰 값을 뽑아낸다. 
+        maxvalue = np.max(self.m_Voxel)  # m_Voxel의 가장 큰 값을 뽑아낸다.
         print(maxvalue)
-        self.m_Voxel = self.m_Voxel/maxvalue # 0.~으로 정규화 된다. 
+        self.m_Voxel = self.m_Voxel/maxvalue  # 0.~으로 정규화 된다.
 
     def NormalizeMM(self):
-        self.m_Voxel = self.m_Voxel.astype(np.float32, copy = False)
-        maxvalue = np.max(self.m_Voxel) # m_Voxel의 가장 큰 값을 뽑아낸다.
-        minvalue = np.min(self.m_Voxel) # m_Voxel의 가장 작은 값을 뽑아낸다.
+        self.m_Voxel = self.m_Voxel.astype(np.float32, copy=False)
+        maxvalue = np.max(self.m_Voxel)  # m_Voxel의 가장 큰 값을 뽑아낸다.
+        minvalue = np.min(self.m_Voxel)  # m_Voxel의 가장 작은 값을 뽑아낸다.
 
         diff = maxvalue - minvalue
         self.m_Voxel = (self.m_Voxel - minvalue)/diff
 
-    def AdjustPixelRangeNormalize(self, Upper): # version 1? 
-        self.m_Voxel = self.m_Voxel.astype(np.float32, copy = False)
+    def AdjustPixelRangeNormalize(self, Upper):  # version 1?
+        self.m_Voxel = self.m_Voxel.astype(np.float32, copy=False)
         minvalue = np.min(self.m_Voxel)
         self.m_Voxel = self.m_Voxel.clip(minvalue, Upper)
 
